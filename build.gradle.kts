fdf3f200a3bcc8dd529b284d14bb814b94787759
scarf <greenscarf005@gmail.com>
2022-12-05 23:51:51 +0900

    build: prepare kotlin migration

--- build.gradle.kts
+++ build.gradle.kts
@@ -21,7 +21,7 @@ java {
 plugins {
     application
     java
-//    kotlin("jvm") version "1.7.20"
+    kotlin("jvm") version "1.8.0-Beta"
 }
 
 repositories {
@@ -29,10 +29,10 @@ repositories {
 }
 
 dependencies {
-//    implementation(kotlin("stdlib-jdk8"))
-//    implementation(kotlin("reflect"))
     compileOnly(fileTree("lib"))
     compileOnly(files("${slayTheSpireInstallDir}/desktop-1.0.jar"))
+    implementation(kotlin("reflect"))
+    implementation(kotlin("stdlib-jdk8"))
 }
 
 sourceSets {
@@ -76,4 +76,12 @@ tasks.register<Copy>("copyJarToWorkshopFolder") {
 
 tasks.withType<KotlinCompile> {
     kotlinOptions.jvmTarget = "1.8"
+}
+val compileKotlin: KotlinCompile by tasks
+compileKotlin.kotlinOptions {
+    jvmTarget = "1.8"
+}
+val compileTestKotlin: KotlinCompile by tasks
+compileTestKotlin.kotlinOptions {
+    jvmTarget = "1.8"
 }

1d058628729d77644a151f6aa82b380479a1a941
scarf <greenscarf005@gmail.com>
2022-12-06 01:25:21 +0900

    build: migrate to intellij (#1)
    * build: migrate to gradle
    
    * refactor: remove deprecated features
    
    * fix: compile to java 1.8
    
    * build: prepare kotlin migration

---
+++ build.gradle.kts
@@ -0,0 +1,87 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+val slayTheSpireInstallDir = "${System.getenv("HOME")}/.local/share/Steam/steamapps/common/SlayTheSpire"
+val modName = "Marisa"
+
+buildscript {
+    repositories {
+        mavenCentral()
+    }
+
+    dependencies {
+        classpath(kotlin("gradle-plugin", version = "1.3.31"))
+    }
+}
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_1_8
+    targetCompatibility = JavaVersion.VERSION_1_8
+}
+
+plugins {
+    application
+    java
+    kotlin("jvm") version "1.8.0-Beta"
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    compileOnly(fileTree("lib"))
+    compileOnly(files("${slayTheSpireInstallDir}/desktop-1.0.jar"))
+    implementation(kotlin("reflect"))
+    implementation(kotlin("stdlib-jdk8"))
+}
+
+sourceSets {
+    main {
+        java.srcDir("src/main")
+//        java {
+//            srcDirs.add(file("src/main/kotlin/"))
+//        }
+    }
+}
+
+tasks.register<Jar>("createJar") {
+    duplicatesStrategy = DuplicatesStrategy.EXCLUDE
+
+    archiveName = "$modName.jar"
+    from(sourceSets.main.get().output) { }
+    dependsOn(configurations.runtimeClasspath)
+    from({
+        configurations.runtimeClasspath.get()
+            .filter { it.name.endsWith("createJar") }
+            .map { zipTree(it) }
+    })
+    from(file("src/ModTheSpire.json"))
+}
+
+tasks.register<Copy>("copyJarToStsMods") {
+    dependsOn("clean")
+    dependsOn("createJar")
+
+    from("build/libs/${modName}.jar")
+    into("${slayTheSpireInstallDir}/mods")
+}
+
+tasks.register<Copy>("copyJarToWorkshopFolder") {
+    dependsOn("clean")
+    dependsOn("createJar")
+
+    from("build/libs/${modName}.jar")
+    into("${slayTheSpireInstallDir}/${modName}/content") // publish to Workshop folder
+}
+
+tasks.withType<KotlinCompile> {
+    kotlinOptions.jvmTarget = "1.8"
+}
+val compileKotlin: KotlinCompile by tasks
+compileKotlin.kotlinOptions {
+    jvmTarget = "1.8"
+}
+val compileTestKotlin: KotlinCompile by tasks
+compileTestKotlin.kotlinOptions {
+    jvmTarget = "1.8"
+}

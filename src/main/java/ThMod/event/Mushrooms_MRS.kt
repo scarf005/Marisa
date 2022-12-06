6b74999e37d950b9fa8cfde44db128fbc9dbd2d4
scarf <greenscarf005@gmail.com>
2022-12-07 00:18:17 +0900

    refactor: migrate `event` to kotlin

--- src/main/java/ThMod/event/Mushrooms_MRS.kt
+++ src/main/java/ThMod/event/Mushrooms_MRS.kt
@@ -1,136 +1,135 @@
-package ThMod.event;
+package ThMod.event
 
-import ThMod.relics.BigShroomBag;
-import ThMod.relics.ShroomBag;
-import com.badlogic.gdx.graphics.Color;
-import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.megacrit.cardcrawl.cards.AbstractCard;
-import com.megacrit.cardcrawl.cards.curses.Parasite;
-import com.megacrit.cardcrawl.core.CardCrawlGame;
-import com.megacrit.cardcrawl.core.Settings;
-import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
-import com.megacrit.cardcrawl.events.AbstractEvent;
-import com.megacrit.cardcrawl.events.RoomEventDialog;
-import com.megacrit.cardcrawl.helpers.CardLibrary;
-import com.megacrit.cardcrawl.helpers.ImageMaster;
-import com.megacrit.cardcrawl.helpers.MonsterHelper;
-import com.megacrit.cardcrawl.localization.EventStrings;
-import com.megacrit.cardcrawl.relics.Circlet;
-import com.megacrit.cardcrawl.relics.OddMushroom;
-import com.megacrit.cardcrawl.vfx.cardManip.ShowCardAndObtainEffect;
-import org.apache.logging.log4j.Logger;
+import ThMod.relics.BigShroomBag
+import ThMod.relics.ShroomBag
+import com.badlogic.gdx.graphics.Color
+import com.badlogic.gdx.graphics.g2d.SpriteBatch
+import com.megacrit.cardcrawl.cards.AbstractCard
+import com.megacrit.cardcrawl.cards.curses.Parasite
+import com.megacrit.cardcrawl.core.CardCrawlGame
+import com.megacrit.cardcrawl.core.Settings
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon
+import com.megacrit.cardcrawl.events.AbstractEvent
+import com.megacrit.cardcrawl.events.RoomEventDialog
+import com.megacrit.cardcrawl.events.exordium.Mushrooms
+import com.megacrit.cardcrawl.helpers.CardLibrary
+import com.megacrit.cardcrawl.helpers.ImageMaster
+import com.megacrit.cardcrawl.helpers.MonsterHelper
+import com.megacrit.cardcrawl.localization.EventStrings
+import com.megacrit.cardcrawl.relics.Circlet
+import com.megacrit.cardcrawl.relics.OddMushroom
+import com.megacrit.cardcrawl.rooms.AbstractRoom.RoomPhase
+import com.megacrit.cardcrawl.vfx.cardManip.ShowCardAndObtainEffect
+import org.apache.logging.log4j.LogManager
 
-import com.megacrit.cardcrawl.rooms.AbstractRoom.RoomPhase;
-import org.apache.logging.log4j.LogManager;
-
-public class Mushrooms_MRS extends AbstractEvent {
-
-  private static final Logger logger = LogManager.getLogger(
-      com.megacrit.cardcrawl.events.exordium.Mushrooms.class.getName());
-  public static final String ID = "Mushrooms_MRS";
-  private static final EventStrings eventStrings;
-  public static final String NAME;
-  public static final String[] DESCRIPTIONS;
-  private static final String[] OPTIONS;
-  private static final String ENC_NAME = "The Mushroom Lair";
-  private Texture fgImg = ImageMaster.loadImage("images/events/fgShrooms.png");
-  private Texture bgImg = ImageMaster.loadImage("images/events/bgShrooms.png");
-  private static final float HEAL_AMT = 0.25F;
-  private static final String HEAL_MSG;
-  private static final String FIGHT_MSG;
-  private int screenNum = 0;
-
-  public Mushrooms_MRS() {
-    this.roomEventText.clear();
-    this.body = DESCRIPTIONS[2];
-    this.roomEventText.addDialogOption(OPTIONS[0]);
-    int temp = (int) ((float) AbstractDungeon.player.maxHealth * 0.25F);
-    this.roomEventText
-        .addDialogOption(OPTIONS[1] + temp + OPTIONS[2], CardLibrary.getCopy("Parasite"));
-    AbstractDungeon.getCurrRoom().phase = RoomPhase.EVENT;
-    this.hasDialog = true;
-    this.hasFocus = true;
-  }
-
-  public void update() {
-    super.update();
-    if (!RoomEventDialog.waitForInput) {
-      this.buttonEffect(this.roomEventText.getSelectedOption());
-    }
-
-  }
-
-  protected void buttonEffect(int buttonPressed) {
-    switch (buttonPressed) {
-      case 0:
-        if (this.screenNum == 0) {
-          AbstractDungeon.getCurrRoom().monsters = MonsterHelper.getEncounter(ENC_NAME);
-          this.roomEventText.updateBodyText(FIGHT_MSG);
-          this.roomEventText.updateDialogOption(0, OPTIONS[3]);
-          this.roomEventText.removeDialogOption(1);
-          AbstractEvent.logMetric("Mushrooms_MRS", "Fought Mushrooms");
-          this.screenNum += 2;
-        } else if (this.screenNum == 1) {
-          this.openMap();
-        } else if (this.screenNum == 2) {
-          if (Settings.isDailyRun) {
-            AbstractDungeon.getCurrRoom().addGoldToRewards(AbstractDungeon.miscRng.random(25));
-          } else {
-            AbstractDungeon.getCurrRoom().addGoldToRewards(AbstractDungeon.miscRng.random(20, 30));
-          }
-
-          if (AbstractDungeon.player.hasRelic("ShroomBag")) {
-            AbstractDungeon.getCurrRoom().addRelicToRewards(new BigShroomBag());
-          } else {
-            if (AbstractDungeon.player.hasRelic("BigShroomBag")) {
-              if (AbstractDungeon.player.hasRelic("OddMushroom")) {
-                AbstractDungeon.getCurrRoom().addRelicToRewards(new Circlet());
-              } else {
-                AbstractDungeon.getCurrRoom().addRelicToRewards(new OddMushroom());
-              }
-            } else {
-              AbstractDungeon.getCurrRoom().addRelicToRewards(new ShroomBag());
-            }
-          }
-
-          this.enterCombat();
-          AbstractDungeon.lastCombatMetricKey = ENC_NAME;
+class Mushrooms_MRS : AbstractEvent() {
+    private val fgImg = ImageMaster.loadImage("images/events/fgShrooms.png")
+    private val bgImg = ImageMaster.loadImage("images/events/bgShrooms.png")
+    override fun update() {
+        super.update()
+        if (!RoomEventDialog.waitForInput) {
+            buttonEffect(roomEventText.selectedOption)
         }
-
-        return;
-      case 1:
-        AbstractCard curse = new Parasite();
-        int healAmt = (int) ((float) AbstractDungeon.player.maxHealth * HEAL_AMT);
-        AbstractEvent
-            .logMetricObtainCardAndHeal("Mushrooms_MRS", "Healed and dodged fight", curse, healAmt);
-        AbstractDungeon.player.heal(healAmt);
-        AbstractDungeon.effectList.add(
-            new ShowCardAndObtainEffect(curse, (float) Settings.WIDTH / 2.0F,
-                (float) Settings.HEIGHT / 2.0F));
-        this.roomEventText.updateBodyText(HEAL_MSG);
-        this.roomEventText.updateDialogOption(0, OPTIONS[4]);
-        this.roomEventText.removeDialogOption(1);
-        this.screenNum = 1;
-        return;
-      default:
-        logger.info("ERROR: case " + buttonPressed + " should never be called");
     }
-  }
 
-  public void render(SpriteBatch sb) {
-    sb.setColor(Color.WHITE);
-    sb.draw(this.bgImg, 0.0F, -10.0F, (float) Settings.WIDTH, 1080.0F * Settings.scale);
-    sb.draw(this.fgImg, 0.0F, -20.0F * Settings.scale, (float) Settings.WIDTH,
-        1080.0F * Settings.scale);
-  }
+    override fun buttonEffect(buttonPressed: Int) {
+        when (buttonPressed) {
+            0 -> {
+                when (screenNum) {
+                    0 -> {
+                        AbstractDungeon.getCurrRoom().monsters = MonsterHelper.getEncounter(ENC_NAME)
+                        roomEventText.updateBodyText(FIGHT_MSG)
+                        roomEventText.updateDialogOption(0, OPTIONS[3])
+                        roomEventText.removeDialogOption(1)
+                        logMetric("Mushrooms_MRS", "Fought Mushrooms")
+                        screenNum += 2
+                    }
 
-  static {
-    eventStrings = CardCrawlGame.languagePack.getEventString("Mushrooms_MRS");
-    NAME = eventStrings.NAME;
-    DESCRIPTIONS = eventStrings.DESCRIPTIONS;
-    OPTIONS = eventStrings.OPTIONS;
-    HEAL_MSG = DESCRIPTIONS[0];
-    FIGHT_MSG = DESCRIPTIONS[1];
-  }
+                    1 -> {
+                        openMap()
+                    }
+
+                    2 -> {
+                        if (Settings.isDailyRun) {
+                            AbstractDungeon.getCurrRoom().addGoldToRewards(AbstractDungeon.miscRng.random(25))
+                        } else {
+                            AbstractDungeon.getCurrRoom().addGoldToRewards(AbstractDungeon.miscRng.random(20, 30))
+                        }
+                        if (AbstractDungeon.player.hasRelic("ShroomBag")) {
+                            AbstractDungeon.getCurrRoom().addRelicToRewards(BigShroomBag())
+                        } else {
+                            if (AbstractDungeon.player.hasRelic("BigShroomBag")) {
+                                if (AbstractDungeon.player.hasRelic("OddMushroom")) {
+                                    AbstractDungeon.getCurrRoom().addRelicToRewards(Circlet())
+                                } else {
+                                    AbstractDungeon.getCurrRoom().addRelicToRewards(OddMushroom())
+                                }
+                            } else {
+                                AbstractDungeon.getCurrRoom().addRelicToRewards(ShroomBag())
+                            }
+                        }
+                        enterCombat()
+                        AbstractDungeon.lastCombatMetricKey = ENC_NAME
+                    }
+                }
+                return
+            }
+
+            1 -> {
+                val curse: AbstractCard = Parasite()
+                val healAmt = (AbstractDungeon.player.maxHealth.toFloat() * HEAL_AMT).toInt()
+                logMetricObtainCardAndHeal("Mushrooms_MRS", "Healed and dodged fight", curse, healAmt)
+                AbstractDungeon.player.heal(healAmt)
+                AbstractDungeon.effectList.add(
+                    ShowCardAndObtainEffect(
+                        curse, Settings.WIDTH.toFloat() / 2.0f,
+                        Settings.HEIGHT.toFloat() / 2.0f
+                    )
+                )
+                roomEventText.updateBodyText(HEAL_MSG)
+                roomEventText.updateDialogOption(0, OPTIONS[4])
+                roomEventText.removeDialogOption(1)
+                screenNum = 1
+                return
+            }
+
+            else -> logger.info("ERROR: case $buttonPressed should never be called")
+        }
+    }
+
+    override fun render(sb: SpriteBatch) {
+        sb.color = Color.WHITE
+        sb.draw(bgImg, 0.0f, -10.0f, Settings.WIDTH.toFloat(), 1080.0f * Settings.scale)
+        sb.draw(
+            fgImg, 0.0f, -20.0f * Settings.scale, Settings.WIDTH.toFloat(),
+            1080.0f * Settings.scale
+        )
+    }
+
+    init {
+        roomEventText.clear()
+        body = DESCRIPTIONS[2]
+        roomEventText.addDialogOption(OPTIONS[0])
+        val temp = (AbstractDungeon.player.maxHealth.toFloat() * 0.25f).toInt()
+        roomEventText
+            .addDialogOption(OPTIONS[1] + temp + OPTIONS[2], CardLibrary.getCopy("Parasite"))
+        AbstractDungeon.getCurrRoom().phase = RoomPhase.EVENT
+        hasDialog = true
+        hasFocus = true
+    }
+
+    companion object {
+        private val logger = LogManager.getLogger(
+            Mushrooms::class.java.name
+        )
+        const val ID = "Mushrooms_MRS"
+        private val eventStrings: EventStrings = CardCrawlGame.languagePack.getEventString("Mushrooms_MRS")
+        val NAME: String? = eventStrings.NAME
+        val DESCRIPTIONS: Array<String> = eventStrings.DESCRIPTIONS
+        private val OPTIONS = eventStrings.OPTIONS
+        private const val ENC_NAME = "The Mushroom Lair"
+        private const val HEAL_AMT = 0.25f
+        private val HEAL_MSG: String = DESCRIPTIONS[0]
+        private val FIGHT_MSG: String = DESCRIPTIONS[1]
+    }
 }
--- src/main/java/ThMod/event/OrinTheCat.kt
+++ src/main/java/ThMod/event/OrinTheCat.kt
@@ -1,182 +1,155 @@
-package ThMod.event;
+package ThMod.event
 
-import static ThMod.ThMod.logger;
+import ThMod.ThMod
+import ThMod.cards.derivations.Wraith
+import ThMod.relics.CatCart
+import com.megacrit.cardcrawl.core.CardCrawlGame
+import com.megacrit.cardcrawl.core.Settings
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon
+import com.megacrit.cardcrawl.events.AbstractEvent
+import com.megacrit.cardcrawl.events.RoomEventDialog
+import com.megacrit.cardcrawl.helpers.CardLibrary
+import com.megacrit.cardcrawl.helpers.MonsterHelper
+import com.megacrit.cardcrawl.monsters.AbstractMonster
+import com.megacrit.cardcrawl.vfx.cardManip.ShowCardAndObtainEffect
 
-import ThMod.ThMod;
-import ThMod.cards.derivations.Wraith;
-import ThMod.relics.CatCart;
-import com.megacrit.cardcrawl.actions.common.EscapeAction;
-import com.megacrit.cardcrawl.core.CardCrawlGame;
-import com.megacrit.cardcrawl.core.Settings;
-import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
-import com.megacrit.cardcrawl.events.AbstractEvent;
-import com.megacrit.cardcrawl.events.RoomEventDialog;
-import com.megacrit.cardcrawl.helpers.CardLibrary;
-import com.megacrit.cardcrawl.helpers.MonsterHelper;
-import com.megacrit.cardcrawl.localization.EventStrings;
-import com.megacrit.cardcrawl.monsters.AbstractMonster;
-import com.megacrit.cardcrawl.rooms.AbstractRoom;
-import com.megacrit.cardcrawl.vfx.cardManip.ShowCardAndObtainEffect;
+class OrinTheCat : AbstractEvent() {
+    private var screen: CurScreen = CurScreen.INTRO
+    private val orin: AbstractMonster?
+    private val satori: Boolean
 
-public class OrinTheCat
-    extends AbstractEvent {
-
-  public static final String ID = "OrinTheCat";
-  private static final EventStrings eventStrings = CardCrawlGame.languagePack.getEventString(ID);
-  public static final String NAME = eventStrings.NAME;
-  private static final String[] DESCRIPTIONS = eventStrings.DESCRIPTIONS;
-  private static final String[] OPTIONS = eventStrings.OPTIONS;
-  private static final String INTRO_MSG = DESCRIPTIONS[0];
-  private CurScreen screen = CurScreen.INTRO;
-  private AbstractMonster orin;
-  private boolean satori;
-
-  private enum CurScreen {
-    INTRO,
-    PRE_COMBAT,
-    END;
-
-    CurScreen() {
+    private enum class CurScreen {
+        INTRO, PRE_COMBAT, END
     }
-  }
 
-  public OrinTheCat() {
-    /*
+    init {
+        /*
     initializeImage(
         "images/events/sphereClosed.png",
         1120.0F * Settings.scale,
         AbstractDungeon.floorY - 50.0F * Settings.scale
     );
 */
-    this.roomEventText.clear();
-    this.body = INTRO_MSG;
-    satori = AbstractDungeon.player.name.equals("Komeiji");
-    if (satori) {
-      this.roomEventText.addDialogOption(OPTIONS[4]);
-    } else {
-      this.roomEventText.addDialogOption(OPTIONS[0], CardLibrary.getCopy("Wraith"));
+        roomEventText.clear()
+        body = INTRO_MSG
+        satori = AbstractDungeon.player.name == "Komeiji"
+        if (satori) {
+            roomEventText.addDialogOption(OPTIONS[4])
+        } else {
+            roomEventText.addDialogOption(OPTIONS[0], CardLibrary.getCopy("Wraith"))
+        }
+        roomEventText.addDialogOption(OPTIONS[1])
+        hasDialog = true
+        hasFocus = true
+        AbstractDungeon.getCurrRoom().monsters = MonsterHelper.getEncounter("Orin")
+        orin = AbstractDungeon.getCurrRoom().monsters.monsters[0]
+        if (orin != null) {
+            ThMod.logger.info("OrinTheCat : orin get : " + orin.name)
+        } else {
+            ThMod.logger.info("OrinTheCat : error : null orin")
+        }
     }
-    this.roomEventText.addDialogOption(OPTIONS[1]);
 
-    this.hasDialog = true;
-    this.hasFocus = true;
-    AbstractDungeon.getCurrRoom().monsters = MonsterHelper.getEncounter("Orin");
-    orin = AbstractDungeon.getCurrRoom().monsters.monsters.get(0);
-    if (orin != null) {
-      logger.info("OrinTheCat : orin get : " + orin.name);
-    } else {
-      logger.info("OrinTheCat : error : null orin");
+    override fun update() {
+        super.update()
+        if (!RoomEventDialog.waitForInput) {
+            buttonEffect(roomEventText.selectedOption)
+        }
     }
-  }
 
-  @Override
-  public void update() {
-    super.update();
-    if (!RoomEventDialog.waitForInput) {
-      buttonEffect(this.roomEventText.getSelectedOption());
+    override fun onEnterRoom() {
+        ThMod.logger.info("OrinTheCat : OnEnterRoom")
+        AbstractDungeon.getCurrRoom().rewards.clear()
     }
-  }
 
-  @Override
-  public void onEnterRoom() {
-    logger.info("OrinTheCat : OnEnterRoom");
-    AbstractDungeon.getCurrRoom().rewards.clear();
-  }
-
-  @Override
-  protected void buttonEffect(int buttonPressed) {
-    switch (this.screen) {
-      case INTRO:
-        switch (buttonPressed) {
-          case 0:
-            if (satori) {
-              this.screen = CurScreen.END;
-              AbstractDungeon.getCurrRoom().spawnRelicAndObtain(
-                  (float) (Settings.WIDTH / 2), (float) (Settings.HEIGHT / 2), new CatCart()
-              );
-              this.roomEventText.updateBodyText(DESCRIPTIONS[3]);
-              this.roomEventText.updateDialogOption(0, OPTIONS[3]);
-              this.roomEventText.clearRemainingOptions();
-              logMetric(ID, "Special");
-              return;
-            } else {
-              logger.info("OrinTheCat : INTRO : Skipping fight!");
-              this.screen = CurScreen.END;
-              /*
+    override fun buttonEffect(buttonPressed: Int) {
+        when (screen) {
+            CurScreen.INTRO -> when (buttonPressed) {
+                0 -> if (satori) {
+                    screen = CurScreen.END
+                    AbstractDungeon.getCurrRoom().spawnRelicAndObtain(
+                        (Settings.WIDTH / 2).toFloat(), (Settings.HEIGHT / 2).toFloat(), CatCart()
+                    )
+                    roomEventText.updateBodyText(DESCRIPTIONS[3])
+                    roomEventText.updateDialogOption(0, OPTIONS[3])
+                    roomEventText.clearRemainingOptions()
+                    logMetric(ID, "Special")
+                    return
+                } else {
+                    ThMod.logger.info("OrinTheCat : INTRO : Skipping fight!")
+                    screen = CurScreen.END
+                    /*
               if (orin != null) {
                 AbstractDungeon.actionManager.addToBottom(new EscapeAction(orin));
               }
-              */
-              this.roomEventText.updateBodyText(DESCRIPTIONS[2]);
-              this.roomEventText.updateDialogOption(0, OPTIONS[3]);
-              this.roomEventText.clearRemainingOptions();
-              AbstractDungeon.effectList.add(
-                  new ShowCardAndObtainEffect(
-                      new Wraith(),
-                      Settings.WIDTH / 2.0F,
-                      Settings.HEIGHT / 2.0F
-                  )
-              );
-              logMetricIgnored(ID);
-              return;
-            }
-          case 1:
-            if (satori) {
-              this.screen = CurScreen.END;
-              /*
+              */roomEventText.updateBodyText(DESCRIPTIONS[2])
+                    roomEventText.updateDialogOption(0, OPTIONS[3])
+                    roomEventText.clearRemainingOptions()
+                    AbstractDungeon.effectList.add(
+                        ShowCardAndObtainEffect(
+                            Wraith(),
+                            Settings.WIDTH / 2.0f,
+                            Settings.HEIGHT / 2.0f
+                        )
+                    )
+                    logMetricIgnored(ID)
+                    return
+                }
+
+                1 -> if (satori) {
+                    screen = CurScreen.END
+                    /*
               if (orin != null) {
                 AbstractDungeon.actionManager.addToBottom(new EscapeAction(orin));
               }
-              */
-              this.roomEventText.updateBodyText(DESCRIPTIONS[4]);
-              this.roomEventText.updateDialogOption(0, OPTIONS[3]);
-              this.roomEventText.clearRemainingOptions();
-              logMetricIgnored(ID);
-              return;
-            } else {
-              logger.info("OrinTheCat : INTRO : Fight!");
-              this.screen = CurScreen.PRE_COMBAT;
-              this.roomEventText.updateBodyText(DESCRIPTIONS[1]);
-              this.roomEventText.updateDialogOption(0, OPTIONS[2]);
-              this.roomEventText.clearRemainingOptions();
-              logMetric(ID, "Fight");
-              return;
+              */roomEventText.updateBodyText(DESCRIPTIONS[4])
+                    roomEventText.updateDialogOption(0, OPTIONS[3])
+                    roomEventText.clearRemainingOptions()
+                    logMetricIgnored(ID)
+                    return
+                } else {
+                    ThMod.logger.info("OrinTheCat : INTRO : Fight!")
+                    screen = CurScreen.PRE_COMBAT
+                    roomEventText.updateBodyText(DESCRIPTIONS[1])
+                    roomEventText.updateDialogOption(0, OPTIONS[2])
+                    roomEventText.clearRemainingOptions()
+                    logMetric(ID, "Fight")
+                    return
+                }
             }
-            /*
-          default:
-            logger.info(
-                "OrinTheCat event : error : key number "
-                    + buttonPressed
-                    + " should never be pressed."
-            );
-            */
-        }
-        break;
-      case PRE_COMBAT:
-        logger.info("OrinTheCat : PreCombat : Adding Reward");
-        AbstractRoom currRoom = AbstractDungeon.getCurrRoom();
-        currRoom.rewards.clear();
-        if(!AbstractDungeon.player.hasRelic("CatCart")){
-          if (Settings.isDailyRun) {
-            currRoom.addGoldToRewards(AbstractDungeon.miscRng.random(50));
-          } else {
-            currRoom.addGoldToRewards(AbstractDungeon.miscRng.random(45, 55));
-          }
-          currRoom.addRelicToRewards(new CatCart());
-        }
 
-        AbstractDungeon.getCurrRoom().eliteTrigger = true;
-        //this.img = ImageMaster.loadImage("images/events/sphereOpen.png");
+            CurScreen.PRE_COMBAT -> {
+                ThMod.logger.info("OrinTheCat : PreCombat : Adding Reward")
+                val currRoom = AbstractDungeon.getCurrRoom()
+                currRoom.rewards.clear()
+                if (!AbstractDungeon.player.hasRelic("CatCart")) {
+                    if (Settings.isDailyRun) {
+                        currRoom.addGoldToRewards(AbstractDungeon.miscRng.random(50))
+                    } else {
+                        currRoom.addGoldToRewards(AbstractDungeon.miscRng.random(45, 55))
+                    }
+                    currRoom.addRelicToRewards(CatCart())
+                }
+                AbstractDungeon.getCurrRoom().eliteTrigger = true
+                //this.img = ImageMaster.loadImage("images/events/sphereOpen.png");
+                ThMod.logger.info("OrinTheCat : PreCombat : Entering combat")
+                enterCombat()
+                AbstractDungeon.lastCombatMetricKey = "Orin"
+            }
 
-        logger.info("OrinTheCat : PreCombat : Entering combat");
-        enterCombat();
-        AbstractDungeon.lastCombatMetricKey = "Orin";
-        break;
-      case END:
-        logger.info("OrinTheCat : end : Opening Map");
-        openMap();
+            CurScreen.END -> {
+                ThMod.logger.info("OrinTheCat : end : Opening Map")
+                openMap()
+            }
+        }
     }
-  }
+
+    companion object {
+        const val ID = "OrinTheCat"
+        private val eventStrings = CardCrawlGame.languagePack.getEventString(ID)
+        val NAME = eventStrings.NAME!!
+        private val DESCRIPTIONS = eventStrings.DESCRIPTIONS
+        private val OPTIONS = eventStrings.OPTIONS
+        private val INTRO_MSG = DESCRIPTIONS[0]
+    }
 }
-
-//public class OrinTheCat \

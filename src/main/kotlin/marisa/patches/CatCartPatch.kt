e9a3f2337a10f2d659aa3a03815a754267a64fdb
scarf <greenscarf005@gmail.com>
2022-12-14 21:18:51 +0900

    refactor: un-nest ifs

--- src/main/kotlin/marisa/patches/CatCartPatch.kt
+++ src/main/kotlin/marisa/patches/CatCartPatch.kt
@@ -6,6 +6,7 @@ import com.evacipated.cardcrawl.modthespire.lib.SpireReturn
 import com.megacrit.cardcrawl.cards.DamageInfo
 import com.megacrit.cardcrawl.characters.AbstractPlayer
 import com.megacrit.cardcrawl.relics.MarkOfTheBloom
+import marisa.MarisaMod
 import marisa.relics.CatCart
 
 @Suppress("unused")
@@ -15,13 +16,17 @@ class CatCartPatch {
         @SpireInsertPatch(rloc = 149)
         @JvmStatic
         fun insert(p: AbstractPlayer, unused: DamageInfo?): SpireReturn<*> {
-            if (p.hasRelic(CatCart.ID) && !p.hasRelic(MarkOfTheBloom.ID)) {
-                if (p.getRelic(CatCart.ID).counter > 0 && !p.hasRelic(MarkOfTheBloom.ID)) {
+            p.getRelic(CatCart.ID)
+                ?.takeUnless { p.hasRelic(MarkOfTheBloom.ID) }
+                ?.takeIf { it.counter > 0 }
+                ?.let {
                     p.currentHealth = 0
-                    p.getRelic(CatCart.ID).onTrigger()
+                    it.onTrigger()
+                    MarisaMod.logger.info(
+                        """MarisaModEventPatch : CatCartPatch: Resurrecting player with ${it.counter} counters"""
+                    )
                     return SpireReturn.Return<Any?>(null)
                 }
-            }
             return SpireReturn.Continue<Any>()
         }
     }
--- src/main/kotlin/marisa/patches/MarisaModEventPatch.kt
+++ src/main/kotlin/marisa/patches/MarisaModEventPatch.kt
@@ -6,6 +6,7 @@ import com.evacipated.cardcrawl.modthespire.lib.SpirePatch
 import com.evacipated.cardcrawl.modthespire.lib.SpirePostfixPatch
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon
 import com.megacrit.cardcrawl.events.AbstractEvent
+import com.megacrit.cardcrawl.events.exordium.Mushrooms
 import com.megacrit.cardcrawl.random.Random
 import marisa.MarisaMod
 import marisa.characters.Marisa
@@ -29,7 +30,7 @@ class MarisaModEventPatch {
             )
             logEvents()
             when (AbstractDungeon.player) {
-                is Marisa -> "MushRooms"
+                is Marisa -> Mushrooms.ID
                 else -> Mushrooms_MRS.ID
             }
                 .also {

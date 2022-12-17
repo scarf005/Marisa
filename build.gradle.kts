da4c9bdc059d429e99a522a5480bc1786686a097
scarf <greenscarf005@gmail.com>
2022-12-17 14:25:26 +0900

    build: use library directly from steam workshop

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,13 +2,22 @@ import org.jetbrains.kotlin.com.google.gson.Gson
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 import java.text.SimpleDateFormat
 
-val slayTheSpireInstallDir = "${System.getenv("HOME")}/.local/share/Steam/steamapps/common/SlayTheSpire"
-val modName = "MarisaMod"
+val modID = "MarisaContinued"
+
+val (gameDir, modTheSpireDir, basemodDir) = run {
+    val homeDir = System.getProperty("user.home")!!
+    val steamDir = "$homeDir/.local/share/Steam/steamapps"
+    val workShopDir = "$steamDir/workshop/content/Slay the Spire"
+    val (mod, base) = 1605060445 to 1605833019
+    Triple(
+        "$steamDir/common/SlayTheSpire",
+        "$workShopDir/$mod",
+        "$workShopDir/$base",
+    )
+}
 
 buildscript {
-    repositories {
-        mavenCentral()
-    }
+    repositories { mavenCentral() }
 
     dependencies {
         classpath(kotlin("gradle-plugin", version = "1.3.31"))
@@ -33,76 +42,60 @@ repositories {
 dependencies {
     implementation(kotlin("reflect"))
     implementation(kotlin("stdlib-jdk8"))
-    implementation("com.google.code.gson:gson:2.10")
 
-    compileOnly(fileTree("lib"))
-    compileOnly(files("${slayTheSpireInstallDir}/desktop-1.0.jar"))
+    compileOnly("com.google.code.gson:gson:2.10")
+    compileOnly(files("$gameDir/desktop-1.0.jar"))
+    compileOnly(fileTree(modTheSpireDir))
+    compileOnly(fileTree(basemodDir))
 }
 
 sourceSets {
-    main {
-        kotlin.srcDir("src/main")
-    }
+    main { kotlin.srcDir("src/main") }
 }
 
 @Suppress("PropertyName")
 data class ModTheSpire(
     val modid: String,
-    val name: String,
-    val author_list: List<String>,
-    val description: String,
-    val dependencies: List<String>,
-    val version: String,
-    val sts_version: String,
-    val update_json: String,
+    val name: String = "Marisa: Continued (霧雨 魔理沙)",
+    val description: String = "Adds Marisa (霧雨 魔理沙) from Touhou Project as a new playable character.",
+    val version: String = SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss").format(System.currentTimeMillis()),
+    val sts_version: String = "12-01-2022",
+    val mts_version: String = "3.30.0",
+    val author_list: List<String> = listOf("Flynn", "Hell", "Hohner257", "Samsara", "scarf005"),
+    val credits: String = "Original mod: https://steamcommunity.com/sharedfiles/filedetails/?id=1614104912",
+    val dependencies: List<String> = listOf("basemod"),
 )
 
-val template = file("src/main/resources/ModTheSpire.template.json")
-val configPath = "src/main/resources/ModTheSpire.json"
-
 val gson = Gson().newBuilder().setPrettyPrinting().create()
-fun updateVersion() {
-    val config = gson.fromJson(template.readText(), ModTheSpire::class.java)
-    val date = SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss").format(System.currentTimeMillis())
-    val newConfig = config.copy(version = date)
-    println("Updating version to $date")
-    file(configPath).writeText(gson.toJson(newConfig))
-}
+
 tasks.register("updateVersion") {
-    updateVersion()
+    file("src/main/resources/ModTheSpire.json")
+        .writeText(gson.toJson(ModTheSpire(modID)))
 }
 
-tasks.register<Jar>("createJar") {
+fun Jar.configure(version: String) {
+    dependsOn(tasks.clean, tasks.jar, version)
     duplicatesStrategy = DuplicatesStrategy.EXCLUDE
-    dependsOn("updateVersion")
-    dependsOn(configurations.runtimeClasspath)
-
-    from(sourceSets.main.get().output) { }
-    from({
-        configurations.runtimeClasspath.get()
-            .filter { it.name.endsWith("createJar") }
-            .map { zipTree(it) }
-    })
-    from(file("src/main/resources/ModTheSpire.json"))
+    from(sourceSets.main.get().output)
+    exclude("ModTheSpire.template.json")
 }
 
-tasks.register<Copy>("toMods") {
-    dependsOn("clean")
-    dependsOn("createJar")
+tasks.register<Jar>("devJar") {
+    configure("updateVersion")
+}
 
-    println("my jar: \"build/libs/${modName}.jar\"")
-    from("build/libs/${modName}.jar")
-    val dest = "${slayTheSpireInstallDir}/mods"
-    println("moving to $dest")
+fun Copy.configure(dest: String) {
+    dependsOn(tasks.clean, "devJar")
+    from("build/libs/${modID}.jar")
     into(dest)
 }
 
-tasks.register<Copy>("toWorkshop") {
-    dependsOn("clean")
-    dependsOn("createJar")
+tasks.register<Copy>("toMods") {
+    configure(dest = "${gameDir}/mods")
+}
 
-    from("build/libs/${modName}.jar")
-    into("${slayTheSpireInstallDir}/${modName}/content") // publish to Workshop folder
+tasks.register<Copy>("toWorkshop") {
+    configure(dest = "${gameDir}/${modID}/content")
 }
 
 tasks.withType<KotlinCompile> {

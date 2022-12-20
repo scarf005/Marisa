ca2e25fff34c71b345ef90edb4a6bf00ae862fc6
scarf <greenscarf005@gmail.com>
2022-12-21 00:18:14 +0900

    docs: generate changelog (#61)
    * build: attach changelog
    
    * docs: add changelog
    
    * build: use pre-generated version
    
    * docs: workshop description as bbcode file
    
    * feat: use changelog in `docs/`
    
    * chore: remove `semver.kts`
    
    now `vergen` package handles changelog creation

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,9 @@
 import org.jetbrains.kotlin.com.google.gson.Gson
-import java.text.SimpleDateFormat
 
 val modID = "MarisaContinued"
 val jarFile = "$buildDir/libs/${modID}.jar"
+val changelog = File("docs/changelog/changelog.md").readText()
+val changeBBCode = File("docs/changelog/changelog.bbcode").readText()
 
 val (gameDir, modTheSpireDir, basemodDir) = run {
     val homeDir = System.getProperty("user.home")!!
@@ -53,12 +54,12 @@ sourceSets {
     main { kotlin.srcDir("src/main") }
 }
 
-@Suppress("PropertyName")
+@Suppress("PropertyName", "LongLine")
 data class ModTheSpire(
     val modid: String,
     val name: String = "Marisa: Continued (霧雨 魔理沙)",
     val description: String = "Adds Marisa (霧雨 魔理沙) from Touhou Project as a new playable character.",
-    val version: String = SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss").format(System.currentTimeMillis()),
+    val version: String,
     val sts_version: String = "12-01-2022",
     val mts_version: String = "3.30.0",
     val author_list: List<String> = listOf("Flynn", "Hell", "Hohner257", "Samsara", "scarf005"),
@@ -69,77 +70,45 @@ data class ModTheSpire(
 data class Config(
     val steamPublishedID: Long = 2902980404,
     val title: String = "Marisa: Continued",
-    val description: String = """
-        [quote=Marisa Kirisame]It ain't magic if it ain't flashy. Danmaku's all about firepower.[/quote]
-
-        Play as your everyday ordinary magician! 
-        
-        [b]Credits[/b]
-        https://steamcommunity.com/sharedfiles/filedetails/?id=1614104912
-        This mod is a continuation of the abandoned mod. Credits goes to the original author.
-        
-        [b]What's Different From Original Mod[/b]
-        [list]
-            [*] Multiple card previews
-            [*] Fixed Korean translations
-            [*] Bugfixes
-        [/list]
-        
-        [b]Read More[/b]
-        [list]
-            [*] [url=https://github.com/scarf005/Marisa]github[/url]
-            [*] [url=https://github.com/scarf005/Marisa/issues?q=is%3Aissue+is%3Aopen+sort%3Aupdated-desc]bug report[/url]
-            [*] [url=https://github.com/scarf005/Marisa/discussions]discussions[/url]
-        [/list]
-    """.trimIndent(),
+    val description: String = File("docs/workshop.bbcode").readText(),
     val visibility: String = "public",
-    val changeNote: String = "",
+    val changeNote: String,
     val tags: List<String> = listOf(
         "Touhou", "Character", "Marisa", "Kirisame Marisa",
         "English", "Simplified Chinese", "Traditional Chinese", "French", "Korean", "Japanese"
     ),
 )
 
-enum class VersionMode(val jar: String) { DEV("devJar"), RELEASE("releaseJar") }
-
 val gson = Gson().newBuilder().disableHtmlEscaping().setPrettyPrinting().create()
 val configFile = file("src/main/resources/ModTheSpire.json")
 
-tasks.register("timestamp") {
-    configFile.writeText(gson.toJson(ModTheSpire("$modID:dev", name = "Marisa: Continued (DEV)")))
-}
 tasks.register("semver") {
-    configFile.writeText(gson.toJson(ModTheSpire(modID, version = "1.3.20")))
+    val config = ModTheSpire(
+        modID,
+        description = """
+            |Adds Marisa (霧雨 魔理沙) from Touhou Project as a new playable character.
+            |
+            |$changelog
+            """.trimMargin(),
+        version = File("docs/changelog/version.txt").readText()
+    )
+
+    configFile.writeText(gson.toJson(config))
 }
 
-fun Jar.configure(version: VersionMode) {
-    dependsOn(
-        tasks.clean, tasks.jar, if (version == VersionMode.DEV) "timestamp" else "semver"
-    )
+tasks.jar {
+    dependsOn("semver")
     duplicatesStrategy = DuplicatesStrategy.EXCLUDE
     from(sourceSets.main.get().output)
-    exclude("ModTheSpire.template.json")
 }
 
-tasks.register<Jar>(VersionMode.DEV.jar) { configure(VersionMode.DEV) }
-tasks.register<Jar>(VersionMode.RELEASE.jar) { configure(VersionMode.RELEASE) }
 
-fun Copy.configure(version: VersionMode) {
-    dependsOn(tasks.clean, version.jar)
-}
-
-tasks.register<Copy>("toMods") {
-    configure(version = VersionMode.DEV)
-    into("$gameDir/mods")
-
-    from("build/libs/${modID}.jar")
-}
-
-tasks.register<Copy>("toWorkshop") {
-    configure(version = VersionMode.RELEASE)
+tasks.register<Copy>("move") {
+    dependsOn(tasks.jar)
     into("${gameDir}/${modID}")
 
     from(jarFile) { into("content") }
     from("docs/thumbnail/image.jpg")
-    file("$gameDir/${modID}/config.json").writeText(gson.toJson(Config()))
+    file("$gameDir/${modID}/config.json")
+        .writeText(gson.toJson(Config(changeNote = changeBBCode)))
 }

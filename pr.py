a1df7e5f102ba1c9519ff7cd479a88eadbb6927d
scarf <greenscarf005@gmail.com>
2023-01-31 23:34:52 +0900

    build: also close issue

--- pr.py
+++ pr.py
@@ -1,9 +1,12 @@
 #!/usr/bin/env python3
 
+import json
 import re
 import sys
 from subprocess import run
-from typing import Final, NewType
+from typing import Final, NewType, TypedDict
+
+from flupy import flu
 
 
 def get_commit_titles():
@@ -25,16 +28,58 @@ def get_title() -> str:
         case _:
             if len(titles := get_commit_titles()) == 1:
                 return titles[0]
-            if (
-                result := input(
-                    "Enter commit title (empty to choose from existing commits): "
-                )
-            ) != "":
+            result = input(
+                "Enter commit title (empty to choose from existing commits): "
+            )
+            if (result) != "":
                 return result
 
             for i, title in enumerate(titles):
                 print(f"[{i}] {title}")
             return titles[int(input("Enter commit number: "))]
+
+
+class Issue(TypedDict):
+    number: int
+    title: str
+
+
+def open_issues() -> list[Issue]:
+    args = ["gh", "issue", "list", "--state", "open", "--json", "number,title"]
+    text = run(args, capture_output=True, check=True).stdout.decode("utf-8")
+    return json.loads(text)
+
+
+def choose_issues(issues: list[Issue]) -> int | None:
+    issue_nums = flu(issues).map(lambda x: x["number"]).collect()
+    for item in issues:
+        number, title = item["number"], item["title"]
+        print(f"[{number}] {title}")
+
+    def invalid():
+        print(
+            "Invalid issue number, choose between "
+            f"{', '.join(map(str, issue_nums))}"
+        )
+
+    while True:
+        try:
+            text = input("Enter issue number (empty to skip): ")
+            if text == "":
+                return None
+            if (result := int(text)) in issue_nums:
+                return result
+            raise ValueError
+        except ValueError:
+            invalid()
+        except KeyboardInterrupt:
+            print("Aborted")
+            sys.exit(0)
+
+
+def get_issue_number() -> int | None:
+    issues = open_issues()
+    return choose_issues(issues)
 
 
 commit_regex = re.compile(r"(?P<type>.+): (?P<subject>.*)")
@@ -60,10 +105,11 @@ commits: Final = run(
     capture_output=True,
     check=True,
 ).stdout.decode("utf-8")
+issue: Final = get_issue_number()
 
 BODY = f"""\
 ## Summary
-- fixes #
+- fixes #{issue or ""}
 
 ## Changelog
 

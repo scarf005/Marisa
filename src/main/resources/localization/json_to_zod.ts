2328f71d9a7dd65e6b643473e7eb3fab2b9134c4
scarf <greenscarf005@gmail.com>
2023-03-04 21:59:53 +0900

    feat: json_to_zod.ts

---
+++ src/main/resources/localization/json_to_zod.ts
@@ -0,0 +1,69 @@
+// deno-lint-ignore-file ban-types
+import { z } from "https://deno.land/x/zod@v3.20.5/mod.ts"
+
+/**
+ * converts an json object into zod schema.
+ * tailored for validating Slay the Spire localization files.
+ *
+ *  original @link https://github.com/rsinohara/json-to-zod
+ *
+ *  differences to original:
+ *  - tuple by default
+ *  - makes all field required
+ *  - forbidden to have additional fields
+ *  - return zod object code instead full program code
+ */
+export function jsonToZodCode(obj: unknown) {
+  function parse(obj: unknown, seen: object[]): string {
+    switch (typeof obj) {
+      case "string":
+        return "z.string()"
+      case "number":
+        return "z.number()"
+      case "bigint":
+        return "z.number().int()"
+      case "boolean":
+        return "z.boolean()"
+      case "object":
+        if (obj === null) {
+          return "z.null()"
+        }
+        if (seen.find((seenObj) => Object.is(seenObj, obj))) {
+          throw "Circular objects are not supported"
+        }
+        seen.push(obj)
+        if (Array.isArray(obj)) {
+          const options = obj
+            .map((elem) => parse(elem, seen))
+          if (options.length >= 0) {
+            return `z.tuple([${options}])`
+          } else {
+            return `z.array(z.unknown())`
+          }
+        }
+        return `z.object({${
+          Object.entries(obj).map(
+            ([k, v]) => `"${k}":${parse(v, seen)}`,
+          )
+        }}).strict()`
+      case "undefined":
+        return "z.undefined()"
+      case "function":
+        return "z.function()"
+      case "symbol":
+      default:
+        return "z.unknown()"
+    }
+  }
+  const result = parse(obj, [])
+  return `${result}.required()`
+}
+
+/** unsafe function that converts an json object into zod schema */
+export function jsonToZod(obj: unknown) {
+  console.log(`return ${jsonToZodCode(obj)}`)
+  const fn = Function("z", `return ${jsonToZodCode(obj)}`)
+
+  // deno-lint-ignore no-explicit-any
+  return fn(z) as z.ZodObject<any>
+}

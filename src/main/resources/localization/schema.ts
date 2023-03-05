02e76f72d97f5b2d6707f9aad02f0bdcb09fc07c
scarf <greenscarf005@gmail.com>
2023-03-05 11:10:06 +0900

    feat: generate schemas

---
+++ src/main/resources/localization/schema.ts
@@ -0,0 +1,67 @@
+import { WalkEntry } from "https://deno.land/std@0.178.0/fs/walk.ts"
+import { jsonToZodCode } from "./json_to_zod.ts"
+import * as log from "https://deno.land/std@0.178.0/log/mod.ts"
+import { z } from "https://deno.land/x/zod@v3.20.5/mod.ts"
+import { zodToJsonSchema } from "npm:zod-to-json-schema"
+import { tsFormatter } from "./dprint.ts"
+import { jsons } from "./common.ts"
+
+const baseCwd = Deno.cwd().endsWith("localization")
+  ? "."
+  : "./src/main/resources/localization"
+const basePath = `${baseCwd}/schemas`
+console.log(basePath)
+await Deno.mkdir(basePath, { recursive: true })
+
+function writeJsonSchema(schemaCode: string, name: string) {
+  // deno-lint-ignore no-explicit-any
+  const zodObject = Function("z", `return ${schemaCode}`)(z) as any
+  const schema = zodToJsonSchema(zodObject)
+
+  // @ts-ignore: $ref errornously added by zod-to-json-schema
+  delete schema["$ref"]
+
+  const file = `${name}.json`
+  log.info(`write  :: ${file}`)
+
+  return Deno.writeTextFile(
+    file,
+    JSON.stringify(schema, null, 2),
+  )
+}
+function writeZodCode(schemaCode: string, name: string) {
+  const file = `${name}.ts`
+  const rawCode = `
+    import { z } from "https://deno.land/x/zod@v3.20.5/mod.ts"
+
+    export const schema = ${schemaCode}
+  `
+  const code = tsFormatter.formatText(file, rawCode)
+  log.info(`write  :: ${file}`)
+  return Deno.writeTextFile(file, code)
+}
+
+async function schemaGen({ path, name }: WalkEntry) {
+  log.info(`read   :: ${path}`)
+  const file = await Deno.readTextFile(path)
+  const json = JSON.parse(file)
+  const schemaCode = jsonToZodCode(json)
+  const base = `${basePath}/${name.replace(".json", "")}`
+
+  await Promise.all([
+    writeJsonSchema(schemaCode, base),
+    writeZodCode(schemaCode, base),
+  ])
+}
+
+async function main() {
+  const eng = await jsons("ENG")
+  eng.forEach(schemaGen)
+}
+
+if (import.meta.main) {
+  const before = performance.now()
+  await main()
+  const after = performance.now()
+  log.info(`Took ${Number((after - before).toPrecision(2))}ms`)
+}

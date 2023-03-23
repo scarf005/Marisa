c0748e415268d24e961daead8a6ecb972d68b88e
scarf <greenscarf005@gmail.com>
2023-03-23 13:59:35 +0900

    feat: utility getter for monsters

--- src/main/kotlin/marisa/Action.kt
+++ src/main/kotlin/marisa/Action.kt
@@ -5,18 +5,28 @@ package marisa
 import com.megacrit.cardcrawl.actions.AbstractGameAction
 import com.megacrit.cardcrawl.actions.common.ApplyPowerAction
 import com.megacrit.cardcrawl.actions.common.RemoveSpecificPowerAction
+import com.megacrit.cardcrawl.characters.AbstractPlayer
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon
+import com.megacrit.cardcrawl.monsters.AbstractMonster
 import com.megacrit.cardcrawl.powers.AbstractPower
 import kotlin.reflect.KClass
 import kotlin.reflect.full.primaryConstructor
 
-val p get() = AbstractDungeon.player
+val p: AbstractPlayer get() = AbstractDungeon.player
+val monsters: ArrayList<AbstractMonster> get() = AbstractDungeon.getCurrRoom().monsters.monsters
 
 fun addToBot(vararg actions: AbstractGameAction) =
     actions.forEach { AbstractDungeon.actionManager.addToBottom(it) }
 
+fun addToBot(actions: List<AbstractGameAction>) =
+    actions.forEach { AbstractDungeon.actionManager.addToBottom(it) }
+
 fun addToTop(vararg actions: AbstractGameAction) =
     actions.forEach { AbstractDungeon.actionManager.addToTop(it) }
+
+fun addToTop(actions: List<AbstractGameAction>) =
+    actions.forEach { AbstractDungeon.actionManager.addToTop(it) }
+
 
 fun ApplyPowerToPlayerAction(powerClass: KClass<out AbstractPower>) =
     powerClass.primaryConstructor?.call(p).let { power -> ApplyPowerAction(p, p, power) }

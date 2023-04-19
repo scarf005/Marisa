1cc66dbe6fda78c031d2df6c9f3f4b92cc3857cc
scarf <greenscarf005@gmail.com>
2023-04-19 14:01:09 +0900

    refactor: use property

--- src/main/kotlin/marisa/action/ShootingEchoAction.kt
+++ src/main/kotlin/marisa/action/ShootingEchoAction.kt
@@ -3,44 +3,39 @@ package marisa.action
 import com.megacrit.cardcrawl.actions.AbstractGameAction
 import com.megacrit.cardcrawl.actions.utility.DiscardToHandAction
 import com.megacrit.cardcrawl.cards.AbstractCard
-import com.megacrit.cardcrawl.cards.DamageInfo.DamageType
 import com.megacrit.cardcrawl.cards.status.Burn
-import com.megacrit.cardcrawl.characters.AbstractPlayer
 import com.megacrit.cardcrawl.core.CardCrawlGame
 import com.megacrit.cardcrawl.core.Settings
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon
 import marisa.MarisaContinued
+import marisa.p
 
 class ShootingEchoAction(private val card: AbstractCard) : AbstractGameAction() {
-    var player: AbstractPlayer = AbstractDungeon.player
-    private val damageTypeForTurn: DamageType
-
     init {
-        damageTypeForTurn = DamageType.NORMAL
         duration = Settings.ACTION_DUR_FAST
         actionType = ActionType.EXHAUST
     }
 
     override fun update() {
         if (duration == Settings.ACTION_DUR_FAST) {
-            if (player.hand.isEmpty) {
+            if (p.hand.isEmpty) {
                 isDone = true
                 return
             }
-            if (player.hand.size() == 1) {
+            if (p.hand.size() == 1) {
                 MarisaContinued.logger.info("ShootingEchoAction : player hand size is 1")
-                val c = player.hand.topCard
+                val c = p.hand.topCard
                 if (c is Burn) {
                     addToBot(
                         DiscardToHandAction(card)
                     )
                 }
-                player.hand.moveToExhaustPile(c)
+                p.hand.moveToExhaustPile(c)
                 isDone = true
                 return
             } else {
                 MarisaContinued.logger.info("ShootingEchoAction : opening hand card select")
-                AbstractDungeon.handCardSelectScreen.open(TEXT[0], 1, false, false)
+                AbstractDungeon.handCardSelectScreen.open(TEXT, 1, false, false)
             }
             tickDuration()
             return
@@ -49,19 +44,9 @@ class ShootingEchoAction(private val card: AbstractCard) : AbstractGameAction() 
             card.returnToHand = false
             for (c in AbstractDungeon.handCardSelectScreen.selectedCards.group) {
                 if (c is Burn) {
-                    /*
-            addToBot(
-                new DiscardToHandAction(card)
-            );
-            */
                     card.returnToHand = true
-                    /*
-            addToBot(
-                new RefreshHandAction()
-            );
-            */
                 }
-                player.hand.moveToExhaustPile(c)
+                p.hand.moveToExhaustPile(c)
             }
             AbstractDungeon.handCardSelectScreen.wereCardsRetrieved = true
             AbstractDungeon.handCardSelectScreen.selectedCards.group.clear()
@@ -76,6 +61,6 @@ class ShootingEchoAction(private val card: AbstractCard) : AbstractGameAction() 
     companion object {
         private val uiStrings = CardCrawlGame.languagePack
             .getUIString("ExhaustAction")
-        val TEXT = uiStrings.TEXT
+        val TEXT = uiStrings.TEXT[0]
     }
 }

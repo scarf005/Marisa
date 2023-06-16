3e4d58816105a7bcf7a8b2c3e7829b41f714d0b2
aleosiss <askirasa@gmail.com>
2023-06-16 03:28:32 -0400

    code cleanup

--- src/main/kotlin/marisa/fx/CollectingQuirkEffect.kt
+++ src/main/kotlin/marisa/fx/CollectingQuirkEffect.kt
@@ -18,25 +18,22 @@ import marisa.fx.lib.FireProjectileEffect.Companion.CollectingQuirkProjectile
 import marisa.fx.lib.SOUND_EFFECT_KEY
 import marisa.fx.lib.VfxGameEffect
 
+private const val MAX_VFX_PROJECTILES = 50
+
 class CollectingQuirkEffect(private val flipped: Boolean, private val monsterX: Float) : VfxGameEffect() {
     private val relics: List<AbstractRelic> = player.relics
-    private var timer = 0.1f
-    private val origDuration = if (Settings.FAST_MODE) 2.0f else 0.5f
     private var relicCounter = 0
 
     init {
-        duration = origDuration
+        duration = if (Settings.FAST_MODE) 2.0f else 0.5f
     }
 
     override fun update() {
-        if (duration == origDuration) {
-            sound.playA(SOUND_EFFECT_KEY, -0.25f - relics.size.toFloat() / 200.0f)
-            screenShake.shake(ScreenShake.ShakeIntensity.HIGH, ScreenShake.ShakeDur.MED, true)
-            effectsQueue.add(BorderLongFlashEffect(Color.SKY))
-        }
+        if (isFirstUpdate()) { start() }
+
         duration -= Gdx.graphics.deltaTime
         timer -= Gdx.graphics.deltaTime
-        if (relicCounter < relics.size && timer < 0.0f) {
+        if (relicCounter < relics.size.coerceAtMost(MAX_VFX_PROJECTILES) && timer < 0.0f) {
             timer += 0.03f
             effectsQueue.add(CollectingQuirkProjectile(relics[relicCounter].img, relics.size, flipped, monsterX))
             relicCounter += 1
@@ -46,13 +43,17 @@ class CollectingQuirkEffect(private val flipped: Boolean, private val monsterX: 
         }
     }
 
+    override fun start() {
+        sound.playA(SOUND_EFFECT_KEY, -0.25f - relics.size.toFloat() / 200.0f)
+        screenShake.shake(ScreenShake.ShakeIntensity.HIGH, ScreenShake.ShakeDur.MED, true)
+        effectsQueue.add(BorderLongFlashEffect(Color.SKY))
+        hasStarted = true
+    }
+
     override fun render(spriteBatch: SpriteBatch) {}
     override fun dispose() {}
 
-
-
     companion object {
-
         fun toVfx(): VFXAction {
             val duration = if (Settings.FAST_MODE) 0.5f else 1.0f
             val monsters = getMonsters()

dc904c9549dc4c05cf758803d3f4fb373517f7d5
scarf <greenscarf005@gmail.com>
2024-03-11 20:09:16 +0900

    refactor: migrate `release.py` to typescript

---
+++ releases.ts
@@ -0,0 +1,36 @@
+import { join } from "https://deno.land/std@0.219.0/path/join.ts"
+import { verifyHardLink } from "./scripts/link/mod.ts"
+import { steam as SteamPath } from "./scripts/paths.ts"
+import $ from "https://deno.land/x/dax/mod.ts"
+
+export const basePath = "docs/changelog"
+
+export const changelogPath = join(basePath, "changelog.md")
+export const version = (await Deno.readTextFile(join(basePath, "version.txt"))).trim()
+
+const changelogText = await Deno.readTextFile(changelogPath)
+const title = changelogText.split("\n")[0].replace("# ", "")
+const body = changelogText.split("\n").slice(1).join("\n")
+
+export const jarPath = "build/libs/MarisaContinued.jar"
+export const stsPath = join(SteamPath, "SlayTheSpire")
+
+const github = async () => {
+  const artifact = `${jarPath}#MarisaContinued-${version}.jar`
+  const command =
+    $`gh release create --verify-tag --title ${title} --notes ${body} v${version} ${artifact}`
+  await command.printCommand()
+  await command.text()
+}
+
+if (import.meta.main) {
+  switch (Deno.args[0]) {
+    case "github": {
+      await github()
+      break
+    }
+    case "steam": {
+      await $`java -jar mod-uploader.jar upload -w MarisaContinued/`.cwd(stsPath)
+    }
+  }
+}
--- scripts/link/mod.ts
+++ scripts/link/mod.ts
@@ -8,23 +8,26 @@ import { image, jar, mod } from "../paths.ts"
 import { noop } from "./noop.ts"
 import { createHardLink, forceCreateLinkTo, hardlinkError } from "./hardlink.ts"
 
+export const verifyHardLink = async (
+  { quiet, check }: { quiet?: true | undefined; check?: true | undefined },
+) => {
+  const log = quiet ? noop : console.log
+  const action = check ? hardlinkError : createHardLink
+  const linkTo = forceCreateLinkTo(log)(action)
+
+  await Promise.all([
+    linkTo(jar)(join(mod, "content")),
+    linkTo(image)(mod),
+  ])
+}
+
 const main = () =>
   new Command()
     .description("Hard link mod jar and image to steam's slay the spire folder")
     .option("-q, --quiet", "Do not output to console. Used in gradle task.")
     .option("-c, --check", "Exit with code 1 on failure instead.")
-    .action(async ({ quiet, check }) => {
-      const log = quiet ? noop : console.log
-      const action = check ? hardlinkError : createHardLink
-      const linkTo = forceCreateLinkTo(log)(action)
-
-      await Promise.all([
-        linkTo(jar)(join(mod, "content")),
-        linkTo(image)(mod),
-      ])
-    })
-    .parse(Deno.args)
+    .action(verifyHardLink)
 
 if (import.meta.main) {
-  main()
+  await main().parse(Deno.args)
 }

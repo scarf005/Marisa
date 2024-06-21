fd382d4d26e2a4574b7dd8d8e5756f564e50de55
scarf <greenscarf005@gmail.com>
2024-06-22 01:18:51 +0900

    build: bump kotlin version to 2.0.0 (#240)
    * build: bump kotlin version to 2.0.0
    
    * docs: add docstring for actions

--- src/main/kotlin/marisa/Action.kt
+++ src/main/kotlin/marisa/Action.kt
@@ -16,18 +16,21 @@ import kotlin.reflect.full.primaryConstructor
 val p: AbstractPlayer get() = AbstractDungeon.player
 val monsters: ArrayList<AbstractMonster> get() = AbstractDungeon.getCurrRoom().monsters.monsters
 
+/** Add actions to bottom of the action queue */
 fun addToBot(vararg actions: AbstractGameAction) =
     actions.forEach { AbstractDungeon.actionManager.addToBottom(it) }
 
+/** Add actions to bottom of the action queue */
 fun addToBot(actions: List<AbstractGameAction>) =
     actions.forEach { AbstractDungeon.actionManager.addToBottom(it) }
 
+/** Add actions to top of the action queue */
 fun addToTop(vararg actions: AbstractGameAction) =
     actions.forEach { AbstractDungeon.actionManager.addToTop(it) }
 
+/** Add actions to top of the action queue */
 fun addToTop(actions: List<AbstractGameAction>) =
     actions.forEach { AbstractDungeon.actionManager.addToTop(it) }
-
 
 fun ApplyPowerToPlayerAction(powerClass: KClass<out AbstractPower>) =
     powerClass.primaryConstructor?.call(p).let { power -> ApplyPowerAction(p, p, power) }

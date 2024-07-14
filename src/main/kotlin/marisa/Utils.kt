4758b91dad32f547d99dcc910f6d874cae492a50
scarf <greenscarf005@gmail.com>
2024-07-14 09:13:52 +0900

    fix: logic error on damage calculation (#250)

--- src/main/kotlin/marisa/Utils.kt
+++ src/main/kotlin/marisa/Utils.kt
@@ -12,7 +12,27 @@ fun countRelic(id: String): Int {
     return 1
 }
 
-fun Iterable<AbstractCard>.exhaustBurns(): Int =
-    this.filterIsInstance<Burn>()
-        .apply { forEach { p.hand.moveToExhaustPile(it) } }
-        .size
+inline fun <T, reified U : T> Iterable<T>.partitionByType(): Pair<List<T>, List<U>> {
+    val first = ArrayList<T>()
+    val second = ArrayList<U>()
+    for (element in this) {
+        if (element is U) second.add(element)
+        else first.add(element)
+    }
+    return Pair(first, second)
+}
+
+/**
+ * Exhaust all [Burn] cards in the [Iterable] and return the number of [Burn] cards exhausted.
+ */
+fun Iterable<Burn>.exhaustBurns() = onEach { p.hand.moveToExhaustPile(it) }
+
+/**
+ * Partition the [Iterable] into two lists, one containing all [AbstractCard]s and the other containing all [Burn]s.
+ * Burns are automatically exhausted.
+ */
+fun Iterable<AbstractCard>.withCardsBurned(): Pair<List<AbstractCard>, List<Burn>> {
+    val (regular, burns) = this.partitionByType<AbstractCard, Burn>()
+    burns.exhaustBurns()
+    return Pair(regular, burns)
+}
